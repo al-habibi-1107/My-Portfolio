b9fa88e65a3360bd8daa9d9600aa11918dd7e057
Kamil <kamilanwar2001@gmail.com>
2020-11-15 22:42:34 +0400

    added image and load animation

--- app.js
+++ app.js
@@ -2,3 +2,23 @@ window.addEventListener("scroll",function(){
    var nav = document.querySelector("nav");
    nav.classList.toggle("sticky",window.scrollY > 0);
 });
+
+var i =0;
+var text = "Hello World ...!\n I'm";
+var speed = 100;
+
+
+
+function typeAnim(){
+   if(i<text.length){
+     document.querySelector("#type-text").innerHTML += text.charAt(i);
+     i++;
+    setTimeout(typeAnim,speed);
+   }
+}
+
+window.onload= function(){
+  
+      setTimeout(typeAnim,500);
+   
+}
